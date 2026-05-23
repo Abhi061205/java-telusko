279d4364c089dc5bdf65da72849c38b60d75ec3c
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2026-05-23 23:06:37 +0530

    initial commit

---
+++ demo.java
@@ -0,0 +1,13 @@
+class demo{
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 20;
+
+        int result = a % 2 == 1 ? b : 40;
+
+        System.out.println(result);
+        
+    
+    }
+}
